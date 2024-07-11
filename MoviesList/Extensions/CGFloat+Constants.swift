169fa49e48c41b40ae035f6f587c463a19b5a3fc
tiago.bastos <tiago.bastos@imaginecurve.com>
2024-07-11 11:21:47 +0100

    remove file header

--- MoviesList/Extensions/CGFloat+Constants.swift
+++ MoviesList/Extensions/CGFloat+Constants.swift
@@ -1,11 +1,3 @@
-//
-//  CGFloat+Constants.swift
-//  CurveTechTask
-//
-//  Created by Saul Liang on 02/11/2020.
-//  Copyright © 2020 Curve. All rights reserved.
-//
-
 import UIKit
 
 extension CGFloat {
