7dd6ca33f95e898a04e5f966943da7fa5c74dd67
tiago.bastos <tiago.bastos@imaginecurve.com>
2026-08-19 08:55:45 +0100

    refresh api key

--- MoviesList/Networking/URL+Helpers.swift
+++ MoviesList/Networking/URL+Helpers.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-private let apiKey = "27fa2971af9beb9591da45e709689aa3"
+private let apiKey = "856885b6a7d193685b50411ad8897ac2"
 
 extension URL {
     static func popularMoviesURL(at page: Int, apiKey: String = apiKey) -> URL {
